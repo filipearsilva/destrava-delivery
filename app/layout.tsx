af3983d27295562153766f804fa3972f2106ebbc
filipearsilva <89862554+filipearsilva@users.noreply.github.com>
2026-09-25 11:38:52 -0300

    Instala Meta Pixel da LP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { TikTok_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import SmoothScroll from "./components/SmoothScroll";
 
@@ -24,6 +25,23 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${tiktokSans.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground font-sans">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','2247189142680465');fbq('track','PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2247189142680465&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <SmoothScroll />
         {children}
       </body>
